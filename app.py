6e731bff2f07c5732149768f21e963d677171421
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-09-14 07:57:55 -0500

    creamos el bluprint de clientes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify, redirect, render_template
 from Config.db import app
+from api.Cliente.apiclientes import ruta_cliente
+
+app.register_blueprint(ruta_cliente, url_prefix = "/api" )
+
 
 @app.route("/")
 def index():
