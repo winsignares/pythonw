f61157b1d3b927b925bd6b61ee7ec9b8632d0a4d
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-09-14 07:30:31 -0500

    se termino las config

--- Config/db.py
+++ Config/db.py
@@ -5,8 +5,12 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 #creamos las credenciales para conectarnos a la bd
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/prestamoul'
+app.config['SQLALCHEMY_TRACK_MODIFACATIONS'] = False
 
 app.secret_key = "WebAvanzada"
 
 #creamos los objetos de bd
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
