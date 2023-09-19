b58e87ba563c0ae8f492c5c9da841d7a94189095
JuanB12 <jcamilobb03@gmail.com>
2023-09-18 19:36:04 -0500

    modelo prestamo con su api

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify, redirect, render_template
 from Config.db import app
 from api.Cliente.apiclientes import ruta_cliente
+from api.Prestamo.apiprestamos import ruta_prestamo
 
 app.register_blueprint(ruta_cliente, url_prefix = "/api" )
-
+app.register_blueprint(ruta_prestamo, url_prefix = "/api" )
 
 @app.route("/")
 def index():
