272c870c9b57607e70e6f36c9e50ab8f975ade3c
Johan Ramírez <johan1271@hotmail.com>
2023-09-20 18:00:16 -0500

    segundo commit Johan Ramirez

--- Proyects/JohanRamirez/app.py
+++ Proyects/JohanRamirez/app.py
@@ -5,10 +5,15 @@ from api.prestamo.apiprestamos import ruta_prestamo
 from api.detallesPrestamo.apidetalles_prestamo import ruta_detalles_prestamo
 from api.prestamista.apiprestamista import ruta_prestamista
 
+
 app.register_blueprint(ruta_cliente, url_prefix = "/api" )
 app.register_blueprint(ruta_prestamo, url_prefix = "/api" )
 app.register_blueprint(ruta_detalles_prestamo, url_prefix = "/api" )
 app.register_blueprint(ruta_prestamista, url_prefix = "/api" )
+
+
+
+
 @app.route("/")
 def index():
     return "Hola Mundo Johan"
