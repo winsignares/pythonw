0bd91842fc9c00aeb078dcf0fea61e4eb0b0f625
MiniLarosa <101002439+MiniLarosa@users.noreply.github.com>
2023-09-21 01:54:20 -0500

    Done

---
+++ Proyects/Sebastian-Insignares/Models/detalles_prestamo.py
@@ -0,0 +1,21 @@
+from Config.db import db, app, ma
+
+class Detalles_prestamo(db.Model):
+    __tablename__ = "tbldetalles_prestamo"
+
+    id = db.Column(db.Integer, primary_key = True)
+    id_prestamo = db.Column(db.Integer, db.ForeignKey('tblprestamo.id'))
+    saldo_pendiente = db.Column(db.Numeric)
+    monto_desembolsado = db.Column(db.Numeric)
+    
+    
+    def __init__(self, id):
+        self.id = id
+
+with app.app_context():
+    db.create_all()
+
+class DetallesPrestamoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_prestamo', 'saldo_pendiente', 'monto_desembolsado')
+        
