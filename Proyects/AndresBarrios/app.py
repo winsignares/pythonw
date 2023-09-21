91b4f9d2efc8e9b5ebd9c008c2faefe47d5201cf
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2023-09-21 07:02:45 -0500

    second commit

--- Proyects/AndresBarrios/app.py
+++ Proyects/AndresBarrios/app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify, redirect, render_template
 from Config.db import app
 from api.Cliente.apiclientes import ruta_cliente
 from api.Prestamo.apiprestamos import ruta_prestamo
-from api.Detalles_prestamo import ruta_detalles_prestamo
-from api.Prestamista import ruta_prestamista
+from api.Detalles_prestamo.apidetalles_prestamo import ruta_detalles_prestamo
+from api.Prestamista.apiprestamista import ruta_prestamista
 
 app.register_blueprint(ruta_cliente, url_prefix = "/api" )
 app.register_blueprint(ruta_prestamista, url_prefix = "/api")
