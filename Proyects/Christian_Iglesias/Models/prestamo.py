94f315a3fffb169e3bf825f8daeadbd65ddfa257
Christian Iglesias <59263708+ChristGG1997@users.noreply.github.com>
2023-09-20 22:32:37 -0500

    Add Project => Christian Iglesias

---
+++ Proyects/Christian_Iglesias/Models/prestamo.py
@@ -0,0 +1,24 @@
+from Config.db import db, app, ma
+
+class Prestamo(db.Model):
+    __tablename__ = "tblprestamo"
+
+    id = db.Column(db.Integer, primary_key = True)
+    id_cliente = db.Column(db.Integer, db.ForeignKey('tblcliente.id'))
+    id_prestamista = db.Column(db.Integer, db.ForeignKey('tblprestamista.id'))
+    fecha_solicitud = db.Column(db.DateTime)
+    monto = db.Column(db.Numeric)
+    tasa_interes = db.Column(db.Numeric)
+    plazo_meses = db.Column(db.Integer)
+    estado = db.Column(db.String(50))
+
+    def __init__(self, id_cliente):
+        self.id_cliente = id_cliente
+
+with app.app_context():
+    db.create_all()
+
+class PrestamoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'id_cliente', 'id_prestamista', 'fecha_solicitud', 'monto', 'tasa_interes', 'plazo_meses', 'estado')
+        
